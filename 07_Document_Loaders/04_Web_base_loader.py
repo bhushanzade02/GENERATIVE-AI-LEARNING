a4ca70646f091b2304507473fbeb6a813ca6c67c
Bhushan Zade <bhushanzade02@gmail.com>
2025-12-25 23:14:35 +0530

    upate

--- 07_Document_Loaders/04_Web_base_loader.py
+++ 07_Document_Loaders/04_Web_base_loader.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
@@ -14,33 +14,19 @@ llm = HuggingFaceEndpoint(
 )
 model = ChatHuggingFace(llm=llm)
 
-
 prompt = PromptTemplate(
-    template = "write the summary for the flowing poem \n {poem}",
-    input_variables = ['poem']
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
 )
-
 
 parser = StrOutputParser()
 
-
-loader = TextLoader("cricket.txt", encoding = "utf-8")
+url = 'https://www.flipkart.com/apple-macbook-air-m2-16-gb-256-gb-ssd-macos-sequoia-mc7x4hn-a/p/itmdc5308fa78421'
+loader = WebBaseLoader(url)
 
 docs = loader.load()
 
-print(type(docs))
-
-
-print(len(docs))
-
-print(docs[0])
-print(docs[0].page_content)
-print(docs[0].metadata)
 
 chain = prompt | model | parser
 
-
-result = chain.invoke({"poem":docs[0].page_content})
-print(result)
-
-
+print(chain.invoke({'question':'What is the prodcut that we are talking about?', 'text':docs[0].page_content}))
